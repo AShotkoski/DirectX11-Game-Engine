3206d380508288010f2af0ed8ed8e1ed1b8c3418
AShotkoski <shotkoskialex@gmail.com>
2023-01-27 17:10:15 -0800

    integrated directxtk and added texture bindable

---
+++ DirectX11 Game Engine/Texture.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Bindable.h"
+#include "Macros.h"
+#include <directxtk/DDSTextureLoader.h>
+#include <string>
+
+class Texture : public Bindable
+{
+public:
+    Texture( Graphics& gfx, const std::wstring& path )
+    {
+        HRESULT hr;  
+       THROW_FAILED_GFX(DirectX::CreateDDSTextureFromFile(
+            pGetDevice( gfx ), path.c_str(), &pResource, &pResourceView));
+
+    }
+    void Bind( Graphics& gfx ) override
+    {
+        pGetContext( gfx )->PSGetShaderResources( 0u, 1u, pResourceView.GetAddressOf() );
+    }
+private:
+    Microsoft::WRL::ComPtr<ID3D11Resource> pResource;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> pResourceView;
+};
+
