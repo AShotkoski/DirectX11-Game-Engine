3cde66c399527309034a90b0fcf8e6f0aad960d1
AShotkoski <shotkoskialex@gmail.com>
2023-01-27 17:16:51 -0800

    added sampler

---
+++ DirectX11 Game Engine/Sampler.cpp
@@ -0,0 +1,20 @@
+#include "Sampler.h"
+#include "Macros.h"
+
+Sampler::Sampler( Graphics& gfx )
+{
+	HRESULT hr;
+
+	D3D11_SAMPLER_DESC sd = {};
+	sd.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_MIP_LINEAR;
+	sd.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sd.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sd.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+
+	THROW_FAILED_GFX( pGetDevice( gfx )->CreateSamplerState( &sd, &pSamplerState ) );
+}
+
+void Sampler::Bind( Graphics& gfx )
+{
+	pGetContext( gfx )->PSSetSamplers( 0u, 1u, pSamplerState.GetAddressOf() );
+}
