5b196ff00edba4798015fd99023be739aff8b986
AShotkoski <shotkoskialex@gmail.com>
2023-02-07 12:59:57 -0800

    Added math util namespace, currently overloading xmfloat3 addition operator.

---
+++ DirectX11 Game Engine/MathUtil.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <DirectXMath.h>
+
+namespace MathUtil
+{
+	
+};
+
+static DirectX::XMFLOAT3 operator+( const DirectX::XMFLOAT3& lhs, const DirectX::XMFLOAT3& rhs )
+{
+	return { lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z };
+}
