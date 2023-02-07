bf163bcb4b9712a806276c4ad6cc1c2481d43e66
AShotkoski <shotkoskialex@gmail.com>
2023-02-07 10:58:32 -0800

    Using a direction setting helper to cleanup code in ray class

--- DirectX11 Game Engine/Ray.h
+++ DirectX11 Game Engine/Ray.h
@@ -17,15 +17,10 @@ public:
 			AddStaticBind( std::make_unique<Binds::Topology>( D3D11_PRIMITIVE_TOPOLOGY_LINELIST ) );
 
 			// ----- Vertices ------
-			dx::XMStoreFloat3(
-				&dir,
-				dx::XMVectorScale(
-					dx::XMVector3Normalize( dx::XMLoadFloat3( &dir ) )
-					, len 
-				)
-			);
 			vertbuf.Emplace_back( source );
 			vertbuf.Emplace_back( dir );
+			// Adjust direction
+			SetDirHelper();
 
 			auto vb = std::make_unique<Binds::DynamicVertexBuffer>( gfx, vertbuf );
 			dynamicVB = vb.get();
@@ -43,21 +38,9 @@ public:
 		AddBind( std::make_unique<Binds::TransformationConstBuffer>( gfx, *this ) );
 	}
 	void SetDir(Graphics& gfx, DirectX::XMFLOAT3 dir)
-	{
-		namespace dx = DirectX;
-		const auto& src = vertbuf[0].Attribute<Vert::VertexLayout::Position_3D>();
-		dx::XMStoreFloat3(
-			&dir,
-			dx::XMVectorScale(
-				dx::XMVector3Normalize( dx::XMLoadFloat3( &dir ) )
-				, len
-			)
-		);
-		dir.x += src.x;
-		dir.y += src.y;
-		dir.z += src.z;
-
+	{	
 		vertbuf[1].Attribute<Vert::VertexLayout::Position_3D>() = dir;
+		SetDirHelper();
 		dynamicVB->Update( gfx, vertbuf );
 	}
 	void SetOrigin(Graphics& gfx, DirectX::XMFLOAT3 origin)
@@ -79,8 +62,21 @@ public:
 		DrawNoIndex( gfx, 2u );
 	}
 private:
+	// Lazy helper class to set direction vector 
+	void SetDirHelper()
+	{
+		namespace dx = DirectX;
+		const auto src = dx::XMLoadFloat3( &vertbuf[0].Attribute<Vert::VertexLayout::Position_3D>() );
+		auto dir = dx::XMLoadFloat3( &vertbuf[1].Attribute<Vert::VertexLayout::Position_3D>() );
+
+		dir = dx::XMVectorScale( dx::XMVector3Normalize( dir ), len );
+		
+		dir = dx::XMVectorAdd( src, dir );
+
+		dx::XMStoreFloat3( &vertbuf[1].Attribute<Vert::VertexLayout::Position_3D>(), dir );
+	}
+private:
 	Vert::VertexBuffer vertbuf;
 	Binds::DynamicVertexBuffer* dynamicVB = nullptr;
 	float len;
 };
-/// MOFO ADD JUST A ORIGIN, AND DIRECTION, AND FIGURE IT OUT FROM THERE. SMH, NO NEW SHADERS IF YOU WILL
