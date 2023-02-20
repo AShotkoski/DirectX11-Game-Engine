1114b37c5ad4764dc63514b32612fcc63cb0114a
AShotkoski <shotkoskialex@gmail.com>
2023-02-19 22:10:06 -0800

    Separated mesh and model code

--- DirectX11 Game Engine/Model.cpp
+++ DirectX11 Game Engine/Model.cpp
@@ -7,26 +7,6 @@
 
 namespace dx = DirectX;
 
-Mesh::Mesh( std::vector<std::shared_ptr<Bindable>>&& binds, Graphics& gfx )
-{
-	AddBind( Binds::TransformationConstBuffer::Resolve( gfx, *this ) );
-	for( auto& b : binds )
-	{
-		AddBind( std::move( b ) );
-	}
- }
-
-void Mesh::BindTransform( DirectX::XMMATRIX Transform )
-{
-	Transform_ = Transform;
-}
-
-DirectX::XMMATRIX Mesh::GetTransformationMatrix() const noexcept
- {
-	 // Return concatenated matrix from all parents
-	 return Transform_;
- }
-
 Node::Node( std::vector<std::shared_ptr<Mesh>> in_meshes, DirectX::XMMATRIX in_transform )
 	 : meshes( std::move( in_meshes ) )
 	 , transform( in_transform )
--- DirectX11 Game Engine/Model.h
+++ DirectX11 Game Engine/Model.h
@@ -2,24 +2,12 @@
 #include "Drawable.h"
 #include "BindableBaseIncludes.h"
 #include "Vertex.h"
+#include "Mesh.h"
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include <memory>
 #include <filesystem>
-
-// In this class it would be a great idea to upgrade the drawing so that we don't need to 
-// re-calculate the concatenated matrices every single call to draw.
-
-class Mesh : public Drawable
-{
-public:
-	Mesh( std::vector<std::shared_ptr<Bindable>>&& binds, Graphics& gfx );
-	void BindTransform( DirectX::XMMATRIX Transform );
-	virtual DirectX::XMMATRIX GetTransformationMatrix() const noexcept override;
-private:
-	DirectX::XMMATRIX Transform_ = DirectX::XMMatrixIdentity();
-};
 
 class Node
 {
