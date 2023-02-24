b6563797da8cef3a23a794f125b2a04414b80062
AShotkoski <shotkoskialex@gmail.com>
2023-02-23 22:30:05 -0800

    added support for unit testing via new proj

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,18 @@
+#include "../Engine/Win.h"
+#include "CppUnitTest.h"
+#include "../Engine/DynamicCBuf.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
