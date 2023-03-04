c8a1a25d315161e0e8c671eb59e7d1c9bb7a7c53
AShotkoski <shotkoskialex@gmail.com>
2023-03-03 16:56:14 -0800

    Improved string conversion funcs

--- Engine/GeneralUtilities.h
+++ Engine/GeneralUtilities.h
@@ -1,25 +1,28 @@
 #pragma once
 #include <string>
+#include "log.h"
 
 namespace Util
 {
-    static std::wstring StringToWString( const std::string& s )
+    static std::wstring StringToWString( const std::string& str )
     {
-        std::wstring temp;
-        for ( auto& c : s )
-        {
-            temp.push_back( (wchar_t)c );
-        }
-        return temp;
+        constexpr const size_t max_supported_strlen = 512u;
+        DCHECK_LT_F( str.length(), max_supported_strlen, "string too long to convert: %s", str.c_str() );
+        wchar_t wide[max_supported_strlen];
+        mbstowcs_s( nullptr, wide, str.c_str(), _TRUNCATE );
+        return wide;
     }
 
-    static std::string WStringToString( const std::wstring& s )
+    static std::string WStringToString( const std::wstring& wstr )
     {
-        std::string temp;
-        for ( auto& c : s )
-        {
-            temp.push_back( (char)c );
-        }
-        return temp;
+        constexpr const size_t max_supported_strlen = 512u;
+        DCHECK_LT_F( wstr.length(), max_supported_strlen, "wstring too long to convert" );
+
+        char narrow[max_supported_strlen];
+        size_t numCharsConverted = 0;
+        wcstombs_s( &numCharsConverted, narrow, wstr.c_str(), _TRUNCATE );
+        // Ensure that the last character is in face a null terminating char
+        narrow[numCharsConverted - 1] = '\0';
+        return narrow;
     }
 }
