e83f9860a878d48bd7a0ee464840627f2a06da28
AShotkoski <shotkoskialex@gmail.com>
2023-03-11 16:14:47 -0800

    changed the "blur" shader to do some random stuff that is less distracting

--- Engine/Shaders/Blur_PS.hlsl
+++ Engine/Shaders/Blur_PS.hlsl
@@ -8,6 +8,17 @@ SamplerState splr;
 
 float4 main(VSOUT psin) : SV_TARGET
 {
+    uint width;
+    uint height;
+    tex.GetDimensions(width, height);
+    float aspectRatio = (float) width / (float) height;
     float4 sampled = tex.Sample(splr, psin.tc);
-    return (0.6f - sampled);
+    float4 sampledinv = 0.5f - sampled;
+    psin.tc = psin.tc - 0.5f;
+    psin.tc.x = psin.tc.x * aspectRatio;
+    psin.tc = psin.tc * 2.f;
+    float distFromCenterSQ = psin.tc.x * psin.tc.x + psin.tc.y * psin.tc.y; // (0 ~ 0.5)
+    float distadjusted = distFromCenterSQ * 35.f;
+    
+    return distadjusted > 0.5f ? sampled : sampledinv;
 }
